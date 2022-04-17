f9be0057b32d68f41f3de1a8b3dcc6662da15cc8
yeongjong <tohero21@naver.com>
2022-04-17 16:32:16 +0900

    Add - 관찰되는 객체의 setter와 getter 설정
    1. setter를 통해 추후 관찰되는 객체가 변경되었음을 인지
    2. 변경 인지 후 실행될 compute를 pulish로 이행하기 위해 publish 함수 선언

--- src/reactive.js
+++ src/reactive.js
@@ -1,7 +1,29 @@
-export function observe() {
-
+export function observe(callback) {
+  publish();
 }
 
-export function observable() {
+export function observable(target) {
+  const PREFIX = "_";
+  const targetEntries = Object.entries(target);
 
+  const state = targetEntries.reduce((acc, [key, value]) => {
+    acc[PREFIX + key] = value;
+    return acc;
+  }, {});
+
+  targetEntries.forEach(([key]) => {
+    Object.defineProperty(state, key, {
+      get: function () {
+        return state[PREFIX + key];
+      },
+      set: function (value) {
+        state[PREFIX + key] = value;
+        publish();
+      },
+    });
+  });
+
+  return state;
 }
+
+function publish() {}
